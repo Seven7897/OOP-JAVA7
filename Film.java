21001377f74b0d9e7b6bcb1ecb0d29d8d71658d1
Anatoly <107132732+Seven7897@users.noreply.github.com>
2022-12-29 17:36:51 +0300

    home 7

---
+++ Film.java
@@ -0,0 +1,19 @@
+package FilmsApp;
+
+public class Film {
+    int filmId;
+    String name;
+    int genreId;
+    int studioId;
+
+    public Film(int filmId, String name, int genreId, int studioId) {
+        this.filmId = filmId;
+        this.studioId = studioId;
+        this.name = name;
+        this.genreId = genreId;
+    }
+
+    public String getFilmName() {
+        return name;
+    }
+}
